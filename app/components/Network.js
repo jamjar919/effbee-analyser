97fcd01987992746e6cfa22e1d999950cc85d58c
James Paterson <james@mjsquare.com>
2019-05-11 17:07:54 +0100

    failed clustering

--- app/components/Network.js
+++ app/components/Network.js
@@ -11,31 +11,140 @@ type Props = {};
 export default class Home extends Component<Props> {
   props: Props;
 
-  componentDidMount() {
-    const friendNodes = new FriendsApi()
-        .get()
-        .map(friend => friend.name)
-        .map(name => ({
-            label: name,
-            id: name
-        }));
-    const nodes = new vis.DataSet(friendNodes);
-    
-    const messageData = new MessagesApi()
-    const edges = new vis.DataSet()
+    constructor(props) {
+        super(props)
+        this.state = {
+            network: false
+        }
+    }
 
-    const container = document.getElementById('network');
-    const data = {
-        nodes,
-        edges
-    };
+    componentDidMount() {
+        const friendNodes = new FriendsApi()
+            .get()
+            .map(friend => friend.name)
+            .map(name => ({
+                label: name,
+                id: name
+            }));
+        const nodes = new vis.DataSet(friendNodes);
+        const messageData = new MessagesApi()
 
-    const network = new vis.Network(container, data, {});
-  }
+        const edgesChats = []
+        for (let i = 0; i < friendNodes.length; i += 1) {
+            for (let j = i + 1; j < friendNodes.length; j += 1) {
+                const f1 = friendNodes[i];
+                const f2 = friendNodes[j];
+                const numChats = messageData.chatsBetween([f1.label, f2.label]).chats.length
+                if (numChats > 0) {
+                    edgesChats.push({
+                        from: f1.label,
+                        to: f2.label,
+                        value: numChats
+                    });
+                }
+            }
+        }
 
-  render() {
-    return (
-      <div id="network" className={styles.network}></div>
-    );
-  }
+        console.log(edgesChats)
+
+        const edges = new vis.DataSet(edgesChats)
+
+        const container = document.getElementById('network');
+        const data = {
+            nodes,
+            edges
+        };
+        const options = {
+            physics: {
+                stabilization:false,
+                repulsion: {
+                    nodeDistance: 1
+                }
+            }
+        }
+
+        var clusterIndex = 0;
+        var clusters = [];
+        var lastClusterZoomLevel = 0;
+        var clusterFactor = 0.5;        
+
+        this.setState({network: new vis.Network(container, data, options)}, () => {
+            const makeClusters = (scale) => {
+                var clusterOptionsByData = {
+                    processProperties: (clusterOptions, childNodes) => {
+                        clusterIndex = clusterIndex + 1;
+                        var childrenCount = 0;
+                        for (var i = 0; i < childNodes.length; i++) {
+                            childrenCount += childNodes[i].childrenCount || 1;
+                        }
+                        clusterOptions.childrenCount = childrenCount;
+                        clusterOptions.label = "# " + childrenCount + "";
+                        clusterOptions.font = {size: childrenCount*5+30}
+                        clusterOptions.id = 'cluster:' + clusterIndex;
+                        clusters.push({id:'cluster:' + clusterIndex, scale:scale});
+                        return clusterOptions;
+                    },
+                    clusterNodeProperties: {borderWidth: 3, shape: 'database', font: {size: 30}}
+                }
+                this.state.network.clusterOutliers(clusterOptionsByData);
+            }
+        
+            const openClusters = (scale) => {
+                var newClusters = [];
+                var declustered = false;
+                for (var i = 0; i < clusters.length; i++) {
+                    if (clusters[i].scale < scale) {
+                        this.state.network.openCluster(clusters[i].id);
+                        lastClusterZoomLevel = scale;
+                        declustered = true;
+                    }
+                    else {
+                        newClusters.push(clusters[i])
+                    }
+                }
+                clusters = newClusters;
+            }
+
+            
+            this.state.network.once('initRedraw', () => {
+                if (lastClusterZoomLevel === 0) {
+                    lastClusterZoomLevel = this.state.network.getScale();
+                }
+            });
+        
+            // we use the zoom event for our clustering
+            this.state.network.on('zoom', function (params) {
+                if (params.direction == '-') {
+                    if (params.scale < lastClusterZoomLevel*clusterFactor) {
+                        makeClusters(params.scale);
+                        lastClusterZoomLevel = params.scale;
+                    }
+                }
+                else {
+                    openClusters(params.scale);
+                }
+            });
+        
+            // if we click on a node, we want to open it up!
+            this.state.network.on("selectNode", function (params) {
+                if (params.nodes.length == 1) {
+                    if (this.state.network.isCluster(params.nodes[0]) == true) {
+                        this.state.network.openCluster(params.nodes[0])
+                    }
+                }
+            });
+        
+        })
+    }
+
+    componentWillUnmount() {
+        this.state.network.destroy();
+        console.log("destroying network...")
+    }
+
+    render() {
+        return (
+        <div id="network" className={styles.network}></div>
+        );
+    }
 }
--- app/facebookapi/messages.js
+++ app/facebookapi/messages.js
@@ -1,12 +1,45 @@
-import FacebookApi from "./api";
 import fs from 'fs';
+import { join } from 'path';
+import FacebookApi from "./api";
 
 class MessagesApi extends FacebookApi {
     constructor() {
         super();
-        const directories = super.getRoot() + '/messages/inbox/';
+        const directories = `${super.getRoot()}/messages/inbox/`;
         const isDirectory = source => fs.lstatSync(source).isDirectory()
-        console.log(directories)
+        const getDirectories = source =>
+            fs.readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+        this.messages = getDirectories(directories).map(directory => {
+            const file = `${directory}/message.json`;
+            const details = JSON.parse(fs.readFileSync(file, { encoding: "utf-8" }))
+            return {
+                participants: details.participants.map(p => ({
+                    name: p.name,
+                    count: details.messages.filter(message => message.sender_name == p.name).length
+                })),
+                title: details.title,
+                file
+            };
+        })
+    }
+
+    chats(name) {
+        return {
+            chats: this.messages.filter(details => (details.participants.map(p => p.name).indexOf(name) >= 0)),
+            name: name
+        }
+    }
+
+    chatsBetween(names) {
+        return {
+            chats: this.messages.filter(details => {
+                const participants = details.participants.map(p => p.name);
+                return (
+                    names.reduce((prev, current) => prev && (participants.indexOf(current) >= 0), true)
+                )
+            }),
+            names
+        }
     }
 }
 
